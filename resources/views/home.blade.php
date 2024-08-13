66caf8542384eec6d11e7704a6a66bb0fea779a9
VladK2706 <lunakevin200627@gmail.com>
2024-08-13 08:13:25 -0500

    Cambios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,83 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="section-form py-4 flex-fill">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">{{ __('Dashboard') }}</div>
 
-                        <div class="card-body">
-                            @if (session('status'))
-                                <div class="alert alert-success" role="alert">
-                                    {{ session('status') }}
-                                </div>
-                            @endif
+<div class="imagen-principal">
+    <img class="" src="{{ asset('images/imagen_principal.png') }}" alt="">
+    <div class="capa-oscura"></div>
+    <div class="text-img-principal">
+        ENCUENTRA LO QUE NECESITES
+    </div>
+</div>
 
-                            {{ __('You are logged in!') }}
-                        </div>
-                    </div>
-                </div>
+
+<div class="nav-bar tipos-productos">
+    <nav class="main-menu tipo-ropa" style="display: block;">
+        <ul>
+
+            <a href="#principal">Blusas</a>
+
+            <a href="#productos">Chaquetas</a>
+
+            <a href="#nosotros">Pantalones</a>
+
+            <a href="#asesoria">Zapatos</a>
+
+        </ul>
+    </nav>
+</div>
+
+<div class="container catalogo">
+    <h1 class="novedades">NOVEDADES</h1>
+    <div class="row align-items-center productos">
+        <div class="col-3 producto">
+            <div class="uni-producto">
+                <a href="producto.html">
+                    <img src="images/faldacuero.jpg" alt="" class="img-producto">
+                    <div class="capa-oscura-producto"></div>
+                </a>
+            </div>
+            <div class="text-product">
+                <a class="nom-producto" href="producto.html">Falda de Cuero</a>
+                <span class="precio">$ 50000</span>
+            </div>
+        </div>
+        <div class="col-3 producto">
+            <div class="uni-producto">
+                <a href="producto.html">
+                    <img src="images/camisacargo.jpg" alt="" class="img-producto">
+                    <div class="capa-oscura-producto"></div>
+                </a>
+            </div>
+            <div class="text-product">
+                <a class="nom-producto" href="producto.html">Chaqueta de Cuero</a>
+                <span class="precio">$ 63000</span>
+            </div>
+        </div>
+        <div class="col-3 producto">
+            <div class="uni-producto">
+                <a href="producto.html">
+                    <img src="images/pantalon mezclilla.webp" alt="" class="img-producto">
+                    <div class="capa-oscura-producto"></div>
+                </a>
+            </div>
+            <div class="text-product">
+                <a class="nom-producto" href="producto.html">Pantalon de mezclilla</a>
+                <span class="precio">$ 55000</span>
+            </div>
+        </div>
+        <div class="col-3 producto">
+            <div class="uni-producto">
+                <a href="producto.html">
+                    <img src="images/af1.webp" alt="" class="img-producto">
+                    <div class="capa-oscura-producto"></div>
+                </a>
+            </div>
+            <div class="text-product">
+                <a class="nom-producto" href="producto.html">Air Force One</a>
+                <span class="precio">$ 100000</span>
             </div>
         </div>
     </div>
+</div>
 @endsection
