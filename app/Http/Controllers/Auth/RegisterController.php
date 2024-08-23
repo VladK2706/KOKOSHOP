2f1d76b4e4dfe339ed91cec3a612eb5e92effd38
VladK2706 <lunakevin200627@gmail.com>
2024-08-22 22:57:33 -0500

    Arreglo de Registro

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,12 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected function redirectTo()
+    {
+        $this->guard()->logout();
+
+        return '/login';
+    }
 
     /**
      * Create a new controller instance.
